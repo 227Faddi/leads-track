50b0e37a790362f3d1392470148cb3073e0389c7
227Faddi <falukhouma27@gmail.com>
2024-11-15 16:52:13 -0500

    chore: change vercel config and views folder for vercel deploy

--- server.js
+++ server.js
@@ -1,66 +1,71 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import passport from 'passport';
-import MongoStore from 'connect-mongo';
-import session from 'express-session';
-import methodOverride from 'method-override';
-import flash from 'express-flash';
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import passport from "passport";
+import MongoStore from "connect-mongo";
+import session from "express-session";
+import methodOverride from "method-override";
+import flash from "express-flash";
 // CONFIG
-import connectDB from './config/database.js';
-import passportConfig from './config/passport.js'
+import connectDB from "./config/database.js";
+import passportConfig from "./config/passport.js";
 // MIDDLEWARE
-import overrideMiddleware from './middleware/method-override.js'
+import overrideMiddleware from "./middleware/method-override.js";
 // ROUTES
-import homeRoutes from './routes/home.js';
-import dashboardRoutes from './routes/dashboard.js';
-import authRoutes from './routes/auth.js';
+import homeRoutes from "./routes/home.js";
+import dashboardRoutes from "./routes/dashboard.js";
+import authRoutes from "./routes/auth.js";
 
-dotenv.config({path: './config/.env'})
+dotenv.config({ path: "./config/.env" });
 
-const app = express()
-connectDB()
+const app = express();
+connectDB();
 
 // Passport Config
-passportConfig(passport)
+passportConfig(passport);
 
 // Body parser
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Sessions
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.DB_STRING })
-}))
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+  })
+);
 
 // Passport Middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Logging
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Static folder and view engine
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+const __dirname = import.meta.dirname;
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public/"));
 
 // Method Override
-app.use(overrideMiddleware.get)
-app.use(flash())
+app.use(overrideMiddleware.get);
+app.use(flash());
 
 // Routes
-app.use('/', homeRoutes)
-app.use('/dashboard', dashboardRoutes)
-app.use('/auth', authRoutes)
+app.use("/", homeRoutes);
+app.use("/dashboard", dashboardRoutes);
+app.use("/auth", authRoutes);
 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
-  
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
+
 // Vercel Deployment
 export default app;
